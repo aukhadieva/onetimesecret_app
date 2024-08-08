ad69e1d06ccee16f513e0532dc6042048fd994f3
Olga Aukhadieva <sarole4ka@gmail.com>
2024-08-08 11:03:16 +0500

    Update database settings add create_tables func

--- database/db.py
+++ database/db.py
@@ -1,12 +1,22 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
 
-engine = create_engine(os.getenv('DATABASE_URL'))
-SessionLocal = sessionmaker()
+engine = create_async_engine(os.getenv('DATABASE_URL'), echo=True)
+AsyncSessionLocal = sessionmaker(bind=engine,class_=AsyncSession,expire_on_commit=False)
 Base = declarative_base()
+
+
+async def create_tables():
+    """
+    Подключается к базе данных и создает таблицы,
+    определенные в моделях, наследующихся от класса Base.
+    :return:
+    """
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
