6a0548569d3edfeeddc5b852a033a6df0d1d96e0
Olga Aukhadieva <sarole4ka@gmail.com>
2024-08-09 18:55:09 +0500

    Update Secret model

--- models/secret.py
+++ models/secret.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from sqlalchemy import Column, Integer, String, CheckConstraint, Enum as EnumType, ForeignKey
+from sqlalchemy import Column, Integer, String, Enum as EnumType, ForeignKey
 from sqlalchemy.orm import relationship
 
 from database.db import Base
@@ -11,7 +11,6 @@ class Lifetime(str, Enum):
     Срок жизни секрета.
     """
     five_min = '5 минут'
-    thirty_min = '30 минут'
     one_hour = '1 час'
     twelve_hours = '12 часов'
     one_day = '1 день'
@@ -26,17 +25,9 @@ class Secret(Base):
     __tablename__ = 'secrets'
     id = Column(Integer, primary_key=True, index=True)
     secret_content = Column(String, nullable=True)
-    secret_password = Column(String, nullable=True)
     passphrase = Column(String, nullable=False)
     lifetime = Column(EnumType(Lifetime), nullable=False)
     link = Column(String)
 
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship('User', back_populates='secrets')
-
-    # Проверка наличия хотя бы одного из полей секрета (secret_content или secret_password)
-    __table_args__ = (
-        CheckConstraint(
-            'secret_content IS NOT NULL OR secret_password IS NOT NULL'
-        ),
-    )
