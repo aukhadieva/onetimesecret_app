48a122b39aec8249ac97831aa42030fb7c5060ab
Olga Aukhadieva <sarole4ka@gmail.com>
2024-08-10 22:57:10 +0500

    Add RefreshToken schema

--- schemas/auth.py
+++ schemas/auth.py
@@ -6,4 +6,12 @@ class Token(BaseModel):
     Модель для описания JWT токенов.
     """
     access_token: str
+    refresh_token: str
     token_type: str
+
+
+class RefreshToken(BaseModel):
+    """
+    Модель для описания JWT токенов для рефреша.
+    """
+    refresh_token: str
