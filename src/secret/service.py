f49709d2a69a1845a289ca4ce80c0200b942ad01
Olga Aukhadieva <sarole4ka@gmail.com>
2024-08-14 21:23:15 +0500

    Update get_secrets endpoint

--- src/secret/service.py
+++ src/secret/service.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from cryptography.fernet import Fernet
 from fastapi import HTTPException
-from fastapi_pagination import Page
+from fastapi_pagination import Page, Params
 from fastapi_pagination.ext.sqlalchemy import paginate
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -19,10 +19,10 @@ async def generate_secret(secret: SecretCreate, user_id: int, db: AsyncSession) 
     """
     Генерирует новый секрет и сохраняет его в базе данных.
 
-    :param secret: объект с данными о новом секрете (типа SecretCreate)
-    :param user_id: идентификатор пользователя (типа int)
-    :param db: экземпляр сессии базы данных (типа AsyncSession)
-    :return: ключ секрета (типа SecretKeyOut)
+    :param secret: объект с данными о новом секрете (тип SecretCreate)
+    :param user_id: идентификатор пользователя (тип int)
+    :param db: экземпляр сессии базы данных (тип AsyncSession)
+    :return: ключ секрета (тип SecretKeyOut)
     """
     secret_content = cipher_suite.encrypt(secret.secret_content)
     passphrase = cipher_suite.encrypt(secret.passphrase)
@@ -38,10 +38,10 @@ async def get_secret(secret_key: bytes, user_id: int, db: AsyncSession) -> Secre
     """
     Получает секрет по зашифрованному ключу и удаляет его из базы данных.
 
-    :param secret_key: зашифрованный ключ секрета (типа bytes)
-    :param user_id: идентификатор пользователя (типа int)
-    :param db: экземпляр сессии базы данных (типа AsyncSession)
-    :return: расшифрованный секрет (типа SecretDecryptOut)
+    :param secret_key: зашифрованный ключ секрета (тип bytes)
+    :param user_id: идентификатор пользователя (тип int)
+    :param db: экземпляр сессии базы данных (тип AsyncSession)
+    :return: расшифрованный секрет (тип SecretDecryptOut)
     """
     query = await db.execute(select(Secret).where((Secret.passphrase == secret_key) & (Secret.user_id == user_id)))
     db_secret = query.scalars().first()
@@ -55,14 +55,15 @@ async def get_secret(secret_key: bytes, user_id: int, db: AsyncSession) -> Secre
     return SecretDecryptOut(secret_content=decrypted_secret)
 
 
-async def get_secrets(user_id: int, db: AsyncSession) -> Page[SecretOut]:
+async def get_secrets(user_id: int, db: AsyncSession, params: Params = Params()) -> Page[SecretOut]:
     """
     Получает список всех секретов (отфильтрованных по убыванию даты создания).
-    Список выводится с возможностью пагинации (50 объектов на странице).
+    Список выводится с возможностью пагинации.
 
     :param user_id: идентификатор пользователя (типа int)
-    :param db: экземпляр сессии базы данных (типа AsyncSession)
-    :return: список секретов (типа Sequence[SecretOut])
+    :param params: параметры пагинации (тип Params)
+    :param db: экземпляр сессии базы данных (тип AsyncSession)
+    :return: список секретов (тип Page[UserOut])
     """
     query = select(Secret).where(Secret.user_id == user_id).order_by(Secret.created_at.desc())
-    return await paginate(db, query)
+    return await paginate(db, query, params)
