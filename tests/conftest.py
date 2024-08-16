d6b0ab92402d4846730876de10076b2dd547e189
Olga Aukhadieva <sarole4ka@gmail.com>
2024-08-16 13:27:14 +0500

    Add test_user fixture and create_test_auth_headers_for_user func

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import asyncio
+import uuid
+from datetime import timedelta
 from typing import AsyncGenerator
 
 import pytest
@@ -6,10 +8,12 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-from src.config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB_TEST
+from src.auth.service import create_access_token
+from src.config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB_TEST, \
+    ACCESS_TOKEN_EXPIRE_MINUTES
 from src.database import get_db, Base
 from src.main import app
-
+from src.user.models import User
 
 DATABASE_URL_TEST = (f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@'
                      f'{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB_TEST}')
@@ -62,3 +66,32 @@ def event_loop() -> AsyncGenerator[asyncio.AbstractEventLoop, None]:
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture(scope='function')
+async def test_user() -> AsyncGenerator[User, None]:
+    """
+    Создает и возвращает тестового пользователя.
+    :return:
+    """
+    unique_email = f'test_email_{uuid.uuid4()}@example.com'
+    user = User(email=unique_email, password='111111')
+    async with AsyncSessionLocal() as session:
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+    yield user
+
+
+def create_test_auth_headers_for_user(email: str) -> dict[str, str]:
+    """
+    Создает заголовки авторизации для пользователя по его email.
+
+    :param email: email пользователя (тип str)
+    :return: заголовки авторизации (тип dict)
+    """
+    access_token = create_access_token(
+        data={"sub": email},
+        expires_delta=timedelta(minutes=float(ACCESS_TOKEN_EXPIRE_MINUTES)),
+    )
+    return {"Authorization": f"Bearer {access_token}"}
